3ee71b9db99c9e8576934ffa892013546a7a1e73
StineNyhus <stnyl@hotmail.com>
2022-03-10 18:05:50 +0100

    added dot in im

--- Where_Is_Wally_exercise/find_wally.py
+++ Where_Is_Wally_exercise/find_wally.py
@@ -11,6 +11,10 @@ import argparse
 import os
 import streamlit as st
 
+####################
+# Define functions #
+####################
+
 def load_image_into_numpy_array(image):
     (im_width, im_height) = image.size
     return np.array(image.getdata()).reshape(
@@ -56,41 +60,101 @@ def prepare_image(image_path):
             # names = [[str(n)] for n in range(300)]
     return image_np, boxes, scores
 
+@st.cache
+def set_values(total: np.array, box: np.array, score: float):
+    """takes a box indicating position on image and an associated score.
+    Adds to the total array the score of the box at the same positions 
+    Returns the total array with new values inserted
 
-def set_values(total, box, score):
+    Args:
+        total (np.array): array storing sum of scores of relevant boxes
+        box (np.array): current box coordinates (xmin, ymin, xmax, ymax)
+        score (float): value between 0 and 1 indicating certainty
+
+    Returns:
+        total (np.array): the updated array
+    """    
     width = total.shape[0]
     height = total.shape[1]
     total[int(np.round(box[0]*width,0)):int(np.round(box[2]*width,0)), 
                 int(np.round(box[1]*height,0)):int(np.round(box[3]*height,0))] += score
     return total
 
-image_n = st.selectbox("Which image do you want to see?",[str(n) for n in range(1,len(os.listdir("images"))-1)])
+def get_mid_point(width, height):
+    return width/2, height/2
 
-image_np, boxes, scores = prepare_image(os.path.join("images", f"{image_n}.jpg"))
+@st.cache
+def plot_pic_dot(image, dot_x, dot_y, alphas, scale_val):
+    fig = plt.figure()
+    plt.imshow(image)
+    plt.imshow(np.ones((image.shape[0], image.shape[1])),  "Greys", alpha=alphas*scale_val)
+    plt.plot(dot_x, dot_y, 'bo')
+    return fig
 
-total = np.zeros((image_np.shape[0], image_np.shape[1]))
-for i, box in enumerate(boxes[0]):
-    # print(box)
-    # shades = np.zeros((image_np.shape[0], image_np.shape[1]))
-    score = scores[0][i]
-    # if score > 0.05:
-    #     score = 0.0
-    total = set_values(total, box, score)
 
-alpha_val = st.slider("Blur image", 0, 100, 0, 1)
+if __name__ == "__main__":
+    st.header("Find Wally in the picture!")
+    st.write("You can blur the image by increasing the slider value.\
+             Move the dot around by pressing the button, and confirm when it points to Wally")
+    image_n = st.selectbox("Which image do you want to see?",[str(n) for n in range(1,len(os.listdir("images")))])
 
-# import plotly.express as px
+    image_np, boxes, scores = prepare_image(os.path.join("images", f"{image_n}.jpg"))
 
-# median = np.median(total)
-alphas = -np.log(total+0.000000000000001)
-alphas_scaled = (alphas - np.min(alphas))/np.ptp(alphas) / 100
+    total = np.zeros((image_np.shape[0], image_np.shape[1]))
+    for i, box in enumerate(boxes[0]):
+        score = scores[0][i]
+        total = set_values(total, box, score)
 
-fig = plt.figure()
-plt.imshow(image_np)
-plt.imshow(np.ones((image_np.shape[0], image_np.shape[1])),  "Greys", alpha=alphas_scaled*alpha_val)
-st.pyplot(fig)
+    # Prepare blurring
+    alpha_val = st.slider("Increase blurring of low probability areas", 0, 100, 0, 1)
+    alphas = -np.log(total+0.000000000000001)
+    alphas_scaled = (alphas - np.min(alphas))/np.ptp(alphas) / 100
 
-np.nonzero(total)
+    # Prepare initial point
+    x_val, y_val = get_mid_point(image_np.shape[0], image_np.shape[1])
+
+    c1, c2, c3, c4 = st.columns(4)
+
+    with c1:
+        x_up = st.button("Increase x position")
+    with c2:
+        x_down = st.button("Decrease x position")
+    with c3:
+        y_up = st.button("Increase y position")
+    with c4:
+        y_down = st.button("Decrease y position")
+
+    if x_up:
+        x_val += 50
+    if x_down:
+        x_val -=  50
+    if y_up:
+        y_val -=  50
+    if y_down:
+        y_val += 50
+
+    fig = plot_pic_dot(image_np, x_val, y_val, alphas_scaled, alpha_val)
+    st.pyplot(fig)
+
+    if st.button("I found Wally") or st.button("I dont think Wally is there"):
+        st.write("Thanks a lot")
+
+
+    # t = np.arange(0.0, 1.0, 0.01)
+    # s = np.sin(2 * np.pi * t)
+    # fig, ax = plt.subplots()
+    # fig.canvas.mpl_connect('button_press_event', onclick)
+    # plt.show()
+    # st.pyplot(fig)
+
+
+
+    # binding_id = plt.connect('motion_notify_event', on_move)
+    # plt.connect('button_press_event', on_click)
+
+    
+
+
 
 
 
